928222f85b51ef520218c2a0dba7d142d252445c
Josh Bodah <jb3689@yahoo.com>
2016-09-28 11:38:45 -0400

    move SuperLogger::Instance out so we have a top-level mod

--- lib/super_logger.rb
+++ lib/super_logger.rb
@@ -2,40 +2,46 @@ require "super_logger/version"
 require "colorized_string"
 require "logger"
 
-class SuperLogger < Logger
-  module Color
-    def self.next
-      color_loop.next
-    end
+module SuperLogger
+  def self.new(*a)
+    Instance.new(*a)
+  end
 
-    def self.color_loop
-      @color_loop ||= Enumerator.new do |y|
-        loop do
-          colors.each { |c| y << c }
+  class Instance < Logger
+    module Color
+      def self.next
+        color_loop.next
+      end
+
+      def self.color_loop
+        @color_loop ||= Enumerator.new do |y|
+          loop do
+            colors.each { |c| y << c }
+          end
         end
+      end
+
+      def self.colors
+        @colors ||= begin
+                      colors = ColorizedString.colors.shuffle
+                      colors.delete(:default)
+                      colors
+                    end
       end
     end
 
-    def self.colors
-      @colors ||= begin
-                    colors = ColorizedString.colors.shuffle
-                    colors.delete(:default)
-                    colors
-                  end
+    def initialize(*a, prefix: nil, use_color: true)
+      super *a
+      @color = SuperLogger::Color.next if use_color
+      @prefix = prefix
     end
-  end
 
-  def initialize(*a, prefix: nil, use_color: true)
-    super *a
-    @color = SuperLogger::Color.next if use_color
-    @prefix = prefix
-  end
-
-  %i(debug info warn error fatal unknown).each do |sym|
-    define_method sym do |msg|
-      msg = "[#{@prefix}] #{msg}" if @prefix
-      msg = ColorizedString[msg].colorize(@color) if @color
-      super msg
+    %i(debug info warn error fatal unknown).each do |sym|
+      define_method sym do |msg|
+        msg = "[#{@prefix}] #{msg}" if @prefix
+        msg = ColorizedString[msg].colorize(@color) if @color
+        super msg
+      end
     end
   end
 end
